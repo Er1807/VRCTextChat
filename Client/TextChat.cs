f10a3747acb009c770229a1c9857ea165817b823
Eric van Fandenfart <ruhlanderic@gmail.com>
2021-09-18 23:00:11 +0200

    Prototype working

--- Client/TextChat.cs
+++ Client/TextChat.cs
@@ -10,6 +10,12 @@ using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Linq;
+using UnityEngine.UI;
+using UnhollowerRuntimeLib;
+using Il2CppSystem.Collections.Generic;
+using VRChatUtilityKit.Components;
+using System.Collections;
 
 [assembly: MelonInfo(typeof(TextChatMod), "TextChat", "1.0.0", "Eric van Fandenfart")]
 [assembly: MelonGame]
@@ -17,58 +23,84 @@ using System.Threading.Tasks;
 namespace TextChat
 {
 
+
     public class TextChatMod : MelonMod
     {
-        private ClientWebSocket ws;
+        Client client;
+        private bool connected = false;
+        private SingleButton button;
 
+        GameObject menu;
         public override void OnApplicationStart()
         {
-            ws = new ClientWebSocket();
-            ws.ConnectAsync(new Uri("ws://localhost:8080"), CancellationToken.None);
-            Task.Run(() => {
-            Console.WriteLine("Task thread ID: {0}",
-                 Thread.CurrentThread.ManagedThreadId);
-                recieve();
-            });
-            VRCActionMenuPage.AddSubMenu(ActionMenuPage.Main,
-                   "Freeze Frame Animation3",
-                   delegate {
-                       MelonLogger.Msg("Freeze Frame Menu Opened");
-                       CustomSubMenu.AddButton("test", () => Test());
-                       CustomSubMenu.AddButton("test3", () => ws.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes("sendMessageTo 1 Hi_from_VRC")), WebSocketMessageType.Text, true, CancellationToken.None));
-                   }
-               );
-
+            client = new Client("ws://localhost:8080");
+            VRCActionMenuPage.AddButton(ActionMenuPage.Main,"SetUserID", () => client.SetUserID());
+            
             MelonLogger.Msg($"Actionmenu initialised");
+
+            client.MessageRecieved += async (userid, message) => {
+                MelonLogger.Msg($"Recieved message from {userid}, with coontent {message}");
+                await AsyncUtils.YieldToMainThread();
+                UiManager.OpenSmallPopup($"Message recieved from {userid}", message, "OK", new Action(() => { }));
+                
+            };
+            client.OnlineRecieved += (userid, online) => { 
+                    if(VRCUtils.ActiveUserInUserInfoMenu?.id == userid && online)
+                        button.ButtonComponent.interactable = true;
+            };
+            VRCUtils.OnUiManagerInit += Init;
         }
 
-        private void Test()
+
+
+        private void Init()
         {
-            string userID = VRCPlayer.field_Internal_Static_VRCPlayer_0.prop_String_2;
-            ws.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes($"startConnection {userID}")), WebSocketMessageType.Text, true, CancellationToken.None);
-            
+            menu = GameObject.Find("UserInterface/MenuContent/Screens/UserInfo/");
+
+            button = new SingleButton(menu,
+                            new Vector3(3, 4), "Send\r\nMessage", delegate
+                            {
+                                GetMessage(VRCUtils.ActiveUserInUserInfoMenu.id);
+                            },
+                            "Send Message to user",
+                            "SendMessageSingleBtn");
+
+            button.gameObject.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+            button.gameObject.transform.localPosition = new Vector3(0, -380, 0);
+
+            EnableDisableListener listener =  menu.AddComponent<EnableDisableListener>();
+
+            listener.OnEnableEvent += () => { RunOnlineCheck(); };
+            MelonLogger.Msg("Buttons sucessfully created");
         }
 
-        private async void recieve()
+       
+
+        
+
+        private void RunOnlineCheck()
         {
-            const int maxMessageSize = 1024;
-            byte[] receiveBuffer = new byte[maxMessageSize];
-            while (ws.State == WebSocketState.Open)
-            {
-                WebSocketReceiveResult receiveResult = await ws.ReceiveAsync(new ArraySegment<byte>(receiveBuffer), CancellationToken.None);
-                if(receiveResult.MessageType == WebSocketMessageType.Text)
-                {
-                    var receivedString = Encoding.UTF8.GetString(receiveBuffer, 0, receiveResult.Count);
-                    MelonLogger.Msg(receivedString);
-                }
-            }
+            button.ButtonComponent.interactable = false;
+            MelonCoroutines.Start(DelyedCheck());
         }
 
-        public override void OnUpdate()
+        public IEnumerator DelyedCheck()
         {
-            
+            yield return null;
+
+            client.IsUserOnline(VRCUtils.ActiveUserInUserInfoMenu.id);
         }
 
+        private void GetMessage(string id)
+        {
+            //https://github.com/Er1807/VRChatVibratorController/blob/main/VRChatVibratorController/VibratorController.cs#L214
+            VRCUiPopupManager.field_Private_Static_VRCUiPopupManager_0.Method_Public_Void_String_String_InputType_Boolean_String_Action_3_String_List_1_KeyCode_Text_Action_String_Boolean_Action_1_VRCUiPopup_Boolean_Int32_0("TextChat", "Message", InputField.InputType.Standard, false/*true = numpad*/, "Send",
+                DelegateSupport.ConvertDelegate<Il2CppSystem.Action<string, List<KeyCode>, Text>>(
+                        (Action<string, List<KeyCode>, Text>)delegate (string message, List<KeyCode> k, Text t)
+                        {
+                            client.SendMessageTo(id, message);
+                        }), new Action(() => { }));
+        }
 
     }
 }
--- Server/server.js
+++ Server/server.js
@@ -23,7 +23,17 @@ wss.on('connection', function connection(ws) {
         try {
             var args = message.split(" ");
             switch (args[0]) {
+                case "ping":
+                    ws.send("pong");
+
                 case "startConnection":
+                    var currentClient = connectedClients.find(o => o.userID === args[1]);
+                    if(currentClient != undefined){
+                        const index = connectedClients.indexOf(currentClient);
+                        if (index > -1) {
+                            connectedClients.splice(index, 1);
+                        }
+                    }
                     var con = new Connection(ws, args[1]);
                     connectedClients.push(new Connection(ws, args[1]));
                     con.send(`connected`);
@@ -32,10 +42,19 @@ wss.on('connection', function connection(ws) {
                 case "sendMessageTo":
                     var currentClient = connectedClients.find(o => o.ws === ws);
                     var remoteClient = connectedClients.find(o => o.userID === args[1]);
-                    if (currentClient !=undefined && remoteClient != undefined) {
+                    if (remoteClient != undefined) {
                         remoteClient.send(`message ${currentClient.userID} ${args[2]}`);
                     } else {
                         currentClient.send('User not online');
+                    }
+                    break;
+                case "isOnline":
+                    var currentClient = connectedClients.find(o => o.ws === ws);
+                    var remoteClient = connectedClients.find(o => o.userID === args[1]);
+                    if (remoteClient != undefined) {
+                        currentClient.send(`status ${args[1]} online`);
+                    } else {
+                        currentClient.send(`status ${args[1]} offline`);
                     }
                     break;
 
